82117a19d7dc5fd86b9f82da68a54636f8006848
Rishibh <r.prakash@mail.utoronto.ca>
2021-11-03 13:57:22 -0400

    Test command added

--- bot.py
+++ bot.py
@@ -1,25 +1,16 @@
 import discord
+from discord.ext import commands
 
 # TODO: Move to a separate file
 TOKEN = "OTA1NDk2NzMzMDE3MDU1MzIy.YYK7jA.gMwm60xlrtLscfh77_rPMeMZg0k"
 GUILD_NAME = "MelodyTest"
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="!")
 
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to discord!')
-    print([guild.name for guild in client.guilds])
-
-    for guild in client.guilds:
-        if guild.name == GUILD_NAME:
-            break
-
-    print(
-        f"{client.user} is connected to the following guilds:\n"
-        f"{guild.name}(id: {guild.id})"
-    )
+@bot.command(name="test")
+async def test(ctx):
+    await ctx.send("it worked!")
 
 
-client.run(TOKEN)
+bot.run(TOKEN)
