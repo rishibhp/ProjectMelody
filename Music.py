5a1e96af10ddd614c32850eddcc662f68a84fc61
Rishibh <r.prakash@mail.utoronto.ca>
2022-02-23 08:40:01 -0500

    Next bug fixed

--- Music.py
+++ Music.py
@@ -227,7 +227,10 @@ class Music(commands.Cog):
         """Command to skip the current track.
         :param ctx: context of the request
         """
-        await self.jump(ctx, "2")
+        if self.queue_index == len(self.queue) - 1:
+            ctx.voice_client.stop()
+        else:
+            await self.jump(ctx, str(self.queue_index + 1))
 
     @commands.command(aliases=["j"])
     async def jump(self, ctx: commands.Context, pos: str) -> None:
